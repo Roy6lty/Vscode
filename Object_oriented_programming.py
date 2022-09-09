1c9dc789d66cae06d27323430ab3855a77bd7b63
Roy6lty <olowoleru06@gmail.com>
2022-09-10 00:52:05 +0100

    Commiing the oop

---
+++ Object_oriented_programming.py
@@ -0,0 +1,15 @@
+class item:
+    pass
+#creating instances of the item class
+item1 = item()
+
+''' Phone Shop'''
+item1 = 'phone'
+item1_price = 100
+iteem1_quality = 5
+item_price_total = item1_price * iteem1_quality
+
+print(item1) #item
+print(type(item.name))
+print(type(item1.price))
+print(type(item.quantity))
