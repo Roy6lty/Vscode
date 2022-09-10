0afd1f967805358f478727c4ce28099a5e593b8f
Roy6lty <olowoleru06@gmail.com>
2022-09-10 01:52:19 +0100

    item2 changes

--- Object_oriented_programming.py
+++ Object_oriented_programming.py
@@ -2,14 +2,18 @@ class item:
     pass
 #creating instances of the item class
 item1 = item()
+item2 = item()
 
 ''' Phone Shop'''
-item1 = 'phone'
-item1_price = 100
-iteem1_quality = 5
+item1_name = 'phone'
+item1_price = 200
+iteem1_quality = 10
 item_price_total = item1_price * iteem1_quality
 
+item2_price = 100
+item2_quantity = 5
+
 print(item1) #item
-print(type(item.name))
-print(type(item1.price))
-print(type(item.quantity))
+print(type(item1_name))
+print(type(item1_price))
+print(type(iteem1_quality))
